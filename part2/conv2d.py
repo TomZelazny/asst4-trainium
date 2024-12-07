b7d2a78791e1953b67636519d626178d7735675f
TomZelazny <Tom.Zelazny@mail.huji.ac.il>
2024-12-07 02:36:45 -0800

    Refactor weight reshaping and buffer allocation in fused_conv2d_maxpool

--- part2/conv2d.py
+++ part2/conv2d.py
@@ -82,30 +82,38 @@ def fused_conv2d_maxpool(X, W, bias, pool_size=1):
     #- transpose that to get an array of shape:
     #   (filter_height, filter_width, n_tiles_c_out, n_tiles_c_in, nl.par_dim(c_in_pmax), c_out_pmax), call this w
     print("<<< W.shape before reshape:", W.shape)
-    W = W.reshape((n_tiles_c_out, c_out_pmax, n_tiles_c_in, c_in_pmax, filter_height, filter_width))
+    # W = W.reshape((n_tiles_c_out, c_out_pmax, n_tiles_c_in, c_in_pmax, filter_height, filter_width))
+    W = W.reshape((out_channels * in_channels, filter_height * filter_width)).T.reshape((filter_height, filter_width, out_channels, in_channels))
     print("<<< W.shape after reshape:", W.shape)
 
-    W_sbuf = nl.ndarray(
-        shape=(n_tiles_c_out, nl.par_dim(c_out_pmax), n_tiles_c_in, c_in_pmax, filter_height, filter_width),
-        dtype=W.dtype,
-        buffer=nl.sbuf
-    )
-
-    for tile_c_out in nl.affine_range(n_tiles_c_out):
-        print("<<< W_sbuf[tile_c_out].shape:", W_sbuf[tile_c_out].shape)
-        print("<<< W[tile_c_out, :, :, :, :, :].shape:", W[tile_c_out, :, :, :, :, :].shape)
-        W_sbuf[tile_c_out] = nl.load(W[tile_c_out, :, :, :, :, :])
+    # W_sbuf = nl.ndarray(
+    #     shape=(n_tiles_c_out, nl.par_dim(c_out_pmax), n_tiles_c_in, c_in_pmax, filter_height, filter_width),
+    #     dtype=W.dtype,
+    #     buffer=nl.sbuf
+    # )
     
-    print("<<< W_sbuf.shape:", W_sbuf.shape)
+    w = nl.ndarray(((filter_height, filter_width, nl.par_dim(out_channels), in_channels)), dtype=W.dtype, buffer=nl.sbuf)
+    w = nl.load(W)
+    print("<<< w.shape:", w.shape)
 
-    w = nl.ndarray(
-        shape=(filter_height, filter_width, n_tiles_c_out, n_tiles_c_in, nl.par_dim(c_in_pmax), c_out_pmax),
-        dtype=W.dtype,
-        buffer=nl.sbuf
-    )
-    for filter_row in nl.affine_range(filter_height):
-        for filter_col in nl.affine_range(filter_width):
-            w[filter_row, filter_col] = nl.copy(W_sbuf[:, :, :, :, filter_row, filter_col], dtype=W.dtype)
+
+    # for tile_c_out in nl.affine_range(n_tiles_c_out):
+    #     print("<<< W_sbuf[tile_c_out].shape:", W_sbuf[tile_c_out].shape)
+    #     print("<<< W[tile_c_out, :, :, :, :, :].shape:", W[tile_c_out, :, :, :, :, :].shape)
+    #     W_sbuf[tile_c_out] = nl.load(W[tile_c_out, :, :, :, :, :])
+    
+    # print("<<< W_sbuf.shape:", W_sbuf.shape)
+
+    # w_temp = nl.ndarray(
+    #     shape=(filter_height, filter_width, n_tiles_c_out, n_tiles_c_in, nl.par_dim(c_out_pmax), c_in_pmax),
+    #     dtype=W.dtype,
+    #     buffer=nl.sbuf
+    # )
+
+    # for filter_row in nl.affine_range(filter_height):
+    #     for filter_col in nl.affine_range(filter_width):
+    #         w_temp[filter_row, filter_col] = nl.copy(W_sbuf[:, :, :, :, filter_row, filter_col])
+
         
     # Process the images in batches
     for b in nl.affine_range(batch_size):
